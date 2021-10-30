70425640911b0b1a48ac4cdd63c749468c72fdaa
Starinin Andrey <starininandrey@gmail.com>
2021-10-30 17:44:59 +0300

    Init

---
+++ MultyCast_Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MultyCast_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            var ip = new IPEndPoint(IPAddress.Any, 8000);
+            socket.Bind(ip);
+            var multicastIP = IPAddress.Parse("224.5.5.5");
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastIP, IPAddress.Any));
+
+            while (true)
+            {
+                var buffer = new byte[256];
+                socket.Receive(buffer);
+                Console.WriteLine(Encoding.Unicode.GetString(buffer));
+            }
+            socket.Close();
+        }
+    }
+}
---
+++ MultyCast_Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MultyCast_Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+            var multicastIP = IPAddress.Parse("224.5.5.5");
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastIP));
+            var ip = new IPEndPoint(multicastIP, 8000);
+            socket.Connect(ip);
+
+            while (true)
+            {
+                socket.Send(Encoding.Unicode.GetBytes("Hello!"));
+            }
+            
+            socket.Close();
+        }
+    }
+}
